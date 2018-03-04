9f27fa70b8b68af68547d515f7f009876f4a9851
dexvillar <dextervillar55@gmail.com>
2018-03-05 00:51:14 +0800

    v1.4

--- aionApp/urls.py
+++ aionApp/urls.py
@@ -6,6 +6,8 @@ app_name = 'aionApp'
 urlpatterns= [
     url(r'^homePage/$', views.homePage, name='homePage'),
     url(r'^registerPage/$', views.registerPage, name='registerPage'),
-    url(r'^logIn/$', views.logIn, name='logIn'),
+    url(r'^homeLogIn/$', views.homeLogIn, name='homeLogIn'),
+    url(r'^shopLogIn/$', views.shopLogIn, name='shopLogIn'),
     url(r'^exitSession/$', views.exitSession, name='exitSession'),
+    url(r'^shopPage/$', views.shopPage, name='shopPage'),
 ]
--- aionApp/views.py
+++ aionApp/views.py
@@ -17,7 +17,7 @@ def homePage(request):
 def registerPage(request):
     return render(request, 'aionApp/register.html')
 
-def logIn(request):
+def homeLogIn(request):
     userList = user.objects.all()
     error = False
     try:
@@ -38,9 +38,42 @@ def logIn(request):
     except ValueError:
         error = True
         return render(request, 'aionApp/home.html', {'error': error})
+    
+def shopLogIn(request):
+    userList = user.objects.all()
+    error = False
+    try:
+        for userTry in userList:
+            if userTry.user_name == str(request.POST['userName']):
+                if userTry.password == str(request.POST['userPassword']):
+                    request.session["user"] = userTry.user_id
+        if request.session["user"] >= 0:
+            currentUser = get_object_or_404(user, user_id = request.session["user"])
+            context = {
+                'currentUser': currentUser,
+            }
+            return render(request, 'aionApp/shop.html', context)
+        else:
+                error = True
+                request.session["user"] = -1
+                return render(request, 'aionApp/shop.html', {'error': error})
+    except ValueError:
+        error = True
+        return render(request, 'aionApp/shop.html', {'error': error})
 
 def exitSession(request):
     request.session['user'] = -1
     return render(request, 'aionApp/home.html')
+
+def shopPage(request):
+    if request.session["user"] > 0:
+        currentUser = get_object_or_404(user, user_id = request.session["user"])
+        context = {
+            'currentUser': currentUser,
+        }
+        return render(request, 'aionApp/shop.html', context)
+    
+    else:
+        return render(request, 'aionApp/shop.html')
     
     
